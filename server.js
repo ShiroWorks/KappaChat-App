8b634f9c7675b85933f01a858ffe7080ff76c504
Szabo Vilmos <vili8899s@gmail.com>
2020-03-24 21:14:15 +0100

    finished

--- server.js
+++ server.js
@@ -72,4 +72,6 @@ io.on('connection', socket => {
 
 const PORT = 3000 || process.env.PORT;
 
-server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+server.listen(process.env.PORT || 5000);
+
+//listen(PORT, () => console.log(`Server running on port ${PORT}`));
